2e518d0601d325b2654453e83eac0b9de2f7ea08
Devarsh Tare <60486774+TareDevarsh@users.noreply.github.com>
2023-12-26 15:18:10 +0100

    Update gptapi.py

--- gptapi/gptapi.py
+++ gptapi/gptapi.py
@@ -4,119 +4,83 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import undetected_chromedriver as uc
-from gtts import gTTS
+from selenium.webdriver.firefox.options import Options
+import socket
+import threading
 import os
-from fake_useragent import UserAgent
-import pyttsx3
 
 
 
-userid = 'ENTER USER ID'
-password = 'ENTER PASSWORD'
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices') 
-engine.setProperty('voice', voices[1].id)
 
 # uc.TARGET_VERSION = 114
 def send_and_read(inputData):
     pass
-dc = DesiredCapabilities.CHROME
-dc['goog:loggingPrefs'] = { 'browser':'ALL' }
 
-ua = UserAgent()
-user_agent = ua.random
-print(user_agent)
 
-option = webdriver.ChromeOptions()
-# option.add_argument(f'user-agent={user_agent}')
-# option.add_argument("--window-size=1920,1080")
-# option.add_argument("--start-maximized")
-# option.add_argument("--no-sandbox")
-# option.add_argument('--disable-blink-features=AutomationControlled')
-# option.add_argument("--remote-debugging-host=127.0.0.1")
-# option.add_experimental_option("useAutomationExtension", False)
-option.add_argument("--disable-gpu")
-option.add_argument("--no-sandbox")
-option.add_argument("--disable-setuid-sandbox")
-# option.add_experimental_option("excludeSwitches",["enable-automation"])
-# option.add_argument("--remote-debugging-port=65139")
-option.add_argument('--disable-logging')
-option.add_argument('--disable-extensions')
-option.add_argument('--disable-infobars')
-option.add_argument('--disable-blink-features=AutomationControlled')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('', 0))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    port = s.getsockname()[1]
 
-# driver = uc.Chrome(headless=True,use_subprocess=False,options=option)
-driver = webdriver.Chrome(options=option)
-driver.get("https://chat.openai.com/chat")
-sleep(5)
-driver.execute_script('''
-        for (const div of document.querySelectorAll('button')) {
-            if (div.textContent.includes('Log in')) {
-                div.click()
-            }
-        }
-         ''')
 
-sleep(5)
+def open_chrome():
+    url = r"https://chat.openai.com"
+    chrome_path = r"/usr/bin/google-chrome"
+    chrome_cmd = f"{chrome_path} --remote-debugging-port={port} --user-data-dir=remote-profile {url}"
+    os.system(chrome_cmd)
 
-if "https://auth0.openai.com/u/login/identifier" in driver.current_url:
-    driver.execute_script('''
-        document.getElementsByClassName('input c10744244 cf50dd0ab')[0].value = "{0}";
-        document.querySelectorAll('[name="action"]')[0].click();
-        '''.format(userid))
-    sleep(2)
-    driver.execute_script('''
-        document.querySelector('[id="password"]').value = "{0}";
-        document.querySelectorAll('[name="action"]')[0].click();
-        '''.format(password))
+chrome_thread = threading.Thread(target=open_chrome)
+chrome_thread.start()
 
-    sleep(5)
-    driver.execute_script('''
-        document.getElementsByClassName('btn relative btn-primary')[1].click()
-        ''')
-    print('We in Boss')
-    sleep(2)
-    inputData = '''
-    Can you pretend that you are my AI assistant called friday? Can your first reply back be only -  "Initializing protocals. Welcome boss, what would you like me to do?"
-    '''
-    sleep(1)
+print("You need to manually complete the log-in or the human verification if required.")
+
+while True:
+    user_input = input(
+        "Enter 'y' if you have completed the log-in or the human verification, or 'n' to check again: ").lower()
+
+    if user_input == 'y':
+        print("Continuing with the automation process...")
+        break
+    elif user_input == 'n':
+        print("Waiting for you to complete the human verification...")
+        time.sleep(5)  # You can adjust the waiting time as needed
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+
+chrome_options = webdriver.ChromeOptions()
+chrome_driver_path = r"/media/dev/ubuntu/code/gptapi/chromedriver/chromedriver"
+chrome_options.add_experimental_option("debuggerAddress", f"127.0.0.1:{port}")
+driver = webdriver.Chrome(options=chrome_options)
+
+sleep(2)
+inputData = '''
+Can you pretend that you are my AI assistant called friday? Can your first reply back be only -  "Initializing protocals. Welcome boss, what would you like me to do?"
+'''
+sleep(1)
+driver.find_element(By.ID, 'prompt-textarea').send_keys(inputData)
+sleep(1)
+driver.find_element(By.ID, 'prompt-textarea').send_keys(Keys.RETURN)
+sleep(7)
+replyLatest = driver.execute_script('''
+return document.getElementsByClassName('w-full text-token-text-primary')[{0}].innerText
+'''.format(1)).replace('ChatGPT', '')
+print(replyLatest)
+os.system(f'pico2wave --lang=en-GB --wave=temp.wav "{replyLatest}"')
+os.system('play temp.wav tempo 1.3')
+
+
+for i in range(1,50,2):
+    inputData = input('--------\nSpeak now: ')
     driver.find_element(By.ID, 'prompt-textarea').send_keys(inputData)
     sleep(1)
     driver.find_element(By.ID, 'prompt-textarea').send_keys(Keys.RETURN)
-    sleep(7)
+    sleep(10)
     replyLatest = driver.execute_script('''
-    return document.getElementsByClassName('group w-full text-token-text-primary border-b border-black/10 dark:border-gray-900/50 bg-gray-50 dark:bg-[#444654]')[{0}].innerText
-    '''.format(0)).replace('ChatGPT', '')
+    return document.getElementsByClassName('w-full text-token-text-primary')[{0}].innerText
+    '''.format(i+2)).replace('ChatGPT', '')
     print(replyLatest)
-    # tts = gTTS(text=replyLatest,
-    #         lang='en',
-    #         slow=False)
-    # tts.save("tts.mp3.mp3")
-    # os.system('tts.mp3 mpg123')
-    engine.say(replyLatest)
-    engine.runAndWait()
+    os.system(f'pico2wave --lang=en-GB --wave=temp.wav "{replyLatest}"')
+    os.system('play temp.wav tempo 1.3')
 
-    for i in range(50):
-        inputData = input('--------\nSpeak now: ')
-        driver.find_element(By.ID, 'prompt-textarea').send_keys(inputData)
-        sleep(1)
-        driver.find_element(By.ID, 'prompt-textarea').send_keys(Keys.RETURN)
-        sleep(10)
-        replyLatest = driver.execute_script('''
-        return document.getElementsByClassName('group w-full text-token-text-primary border-b border-black/10 dark:border-gray-900/50 bg-gray-50 dark:bg-[#444654]')[{0}].innerText
-        '''.format(i+1)).replace('ChatGPT', '')
-        print(replyLatest)
-        # tts = gTTS(text=replyLatest,
-        #            lang='en',
-        #             slow=False)
-        # tts.save("tts.mp3")
-        # os.system('tts.mp3 mpg123')
-        engine.say(replyLatest)
-        engine.runAndWait()
-
-
-
-
+    
 driver.quit()
